013d96e42a463c5e6abcc3c34a1e094abee69836
iamGodsent <iamgodsentt@gmail.com>
2025-10-25 22:31:41 +0100

    added and completed the CRUD functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,50 @@
-import React from 'react'
-import Home from './Pages/Home'
-import Form1 from './Pages/Form1'
-import Hero from './Pages/Hero'
-import {Routes, Route, Link} from "react-router-dom"
 
 
-function App() {
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header' 
+import AddBook from './components/AddBook';
+import BooksList from './components/BooksList'
+import useLocalStorage from './hooks/useLocalStorage'
+import EditBook from './components/EditBook';
+
+
+
+
+
+
+const App = () => {
+  const [books, setBooks] = useLocalStorage('books', []);
+
   return (
     <>
-  
-
-
-    <Routes>
-   <Route path='/' element={<Form1/>}/> 
-    {/* <Route path='/' element={<Hero/>}/> */}
-      <Route path='/home' element={<Home/>}/>
-    </Routes>
-
-      
+      <Header />
+      <div className="main-content">
+        <Routes>
+          <Route
+            path="/"
+            element={<BooksList books={books} setBooks={setBooks} />}
+          />
+          <Route
+            path="/add"
+            element={<AddBook books={books} setBooks={setBooks} />}
+          />
+          <Route
+            path="/edit/:id"
+            element={<EditBook books={books} setBooks={setBooks} />}
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+
+
+
+
+
+
---
+++ src/components/EditBook.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import BookForm from './BookForm';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const EditBook = ({ books, setBooks }) => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const bookToEdit = books.find((book) => book.id === id);
+
+  const handleOnSubmit = (book) => {
+    const filteredBooks = books.filter((b) => b.id !== id);
+    setBooks([book, ...filteredBooks]);
+    navigate('/');
+  };
+
+  return (
+    <div>
+      <BookForm book={bookToEdit} handleOnSubmit={handleOnSubmit} />
+    </div>
+  );
+};
+
+export default EditBook;
